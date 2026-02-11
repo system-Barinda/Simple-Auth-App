0978112c9087bcd73deb302cecb4fbc5570231f2
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-11 17:57:39 +0200

    done

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 
-import Navbar from "./components/layout/Navbar.jsx";
-import Footer from "./components/layout/Footer.jsx";
-import DashBoard from "./components/Dashboard.jsx";
+import Navbar from "./components/layout/Navbar.js";
+import Footer from "./components/layout/Footer.js";
+import DashBoard from "./components/Dashboard.js";
 
 import './App.css'
 
