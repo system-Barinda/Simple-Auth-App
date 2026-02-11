a8281600ea2e2390bde51153e7d4028398c3caa2
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-11 17:59:29 +0200

    done

--- vite-project/src/components/Dashboard.tsx
+++ vite-project/src/components/Dashboard.tsx
@@ -45,25 +45,6 @@ return(
 }
 
 
-
-function ProductCard({product}){
-    return(
-     <div className="border p-3 rounded">
-        <img src={product.image} 
-        alt={product.title}
-        className="h-40 w-full object-contain"
-         />
-
-         <h3 className="text-sm font-semibold mt-2">
-            {product.title}
-         </h3>
-         <p className="text-green-600 font-bold">
-            {product.price}
-         </p>
-     </div>
-    );
-}
-
 function Dashboard(){
    
     return(
---
+++ vite-project/src/components/ProductCard.tsx
@@ -0,0 +1,18 @@
+function ProductCard({product}){
+    return(
+     <div className="border p-3 rounded">
+        <img src={product.image} 
+        alt={product.title}
+        className="h-40 w-full object-contain"
+         />
+
+         <h3 className="text-sm font-semibold mt-2">
+            {product.title}
+         </h3>
+         <p className="text-green-600 font-bold">
+            {product.price}
+         </p>
+     </div>
+    );
+}
+export default ProductCard;
