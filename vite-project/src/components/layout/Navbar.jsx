cb10f40a8e20b9047050bccaffe7028247a14cf4
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-11 16:22:08 +0200

    done

--- vite-project/src/components/layout/Navbar.jsx
+++ vite-project/src/components/layout/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 
-function Navbar(){
-    return(
-        <>
-         <h1>my name is barinda</h1>
-        </>
+
+const menu = [
+'home',
+'service',
+'about',
+'register'
+];
+
+function Navbar(){ 
+let b = menu.map(a => {
+return (
+<div className="bg-slate-200 h-10 w-30 rounded shadow text-stone-900 text-center cursor-pointer" key={a}>{a}</div>
     )
+
+ });
+
+   
+return(
+ <div className="bg-gray-300 h-20 w-[100%] flex justify-center items-center gap-10">
+  {b}
+</div>
+)
 }
 export default Navbar;
