fb84d4575f2caf5237f63c70a1561f931c0d0326
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-11 16:28:09 +0200

    done

---
+++ vite-project/src/components/layout/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer className="bg-gray-900 text-gray-300 py-4">
+            <div className="flex justify-between px-6 text-sm">
+                <span>© 2026 myWebsite</span>
+            </div>
+        </footer>
+    )
+}
