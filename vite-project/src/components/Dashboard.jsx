960bba45f959ec7fb204808ebee7048221a0f5bb
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-11 16:57:41 +0200

    done

--- vite-project/src/components/Dashboard.jsx
+++ vite-project/src/components/Dashboard.jsx
@@ -1,3 +1,14 @@
+function ProductCard({product}){
+    return(
+     <div className="border p-3 rounded">
+        <img src={product.image} 
+        alt={product.title}
+        className="h-40 w-full object-contain"
+         />
+     </div>
+    );
+}
+
 function Dashboard(){
     return(
         <h1>barinda system sylvere</h1>
