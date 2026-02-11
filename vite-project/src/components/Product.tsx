5aa59495a49923567a5242720031a77a55e07da3
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-11 18:01:10 +0200

    done

---
+++ vite-project/src/components/Product.tsx
@@ -0,0 +1,42 @@
+function Product(){
+ const [products,setProducts] = useState<ProductDataType[]>([]);
+ const [error,setError] = useState<string | null>(null);
+ const [loading,setLoading] = useState<boolean>(false);
+
+ const fetchProduct = async() => {
+
+ }
+ useEffect( () => {
+    const fetchProduct = async() => {
+    try{
+   setLoading(true)
+   setError(null)
+
+   let data = await fetch(`${productsUrl}`);
+   if(!data.ok) throw new Error('network connection failed');
+   let corrected: ProductDataType[] = await data.json();
+   setProducts(corrected);
+ 
+  }
+  catch(err){
+   setError(err instanceof Error ? err.message : "Ooops! Something went wrong" )
+  }
+  finally{
+    setLoading(false);
+  }
+};
+fetchProduct();
+ },[]);
+
+return(
+    <div>
+        {loading && <p>loading..............</p>}
+        {error && <p className="text-red-500">{error}</p>}
+
+        {products.map(product => (
+            <div key={product.id}>{product.title}</div>
+        ))}
+    </div>
+)
+}
+export default Product;
