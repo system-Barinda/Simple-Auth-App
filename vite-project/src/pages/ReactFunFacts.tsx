dd9bcdf36eefae7b48cb81df1692ad196ba19757
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-12 17:05:17 +0200

    done

--- vite-project/src/pages/ReactFunFacts.tsx
+++ vite-project/src/pages/ReactFunFacts.tsx
@@ -1,11 +1,11 @@
-import image from "../assets/react.svg"
+import barinda from "../assets/react.svg"
 function Header(){
     return(
-        <header className="h-[91px] w-[550px] bg-[#21222A] text-[#61DAFB]   flex">
+        <header className="h-22.75 w-137.5 bg-[#21222A] text-[#61DAFB]   flex">
 
-            <img src={image} alt="react image" className="w-[28.93px] h-[28.93px] mt-[31px] ml-[26px]" />
+            <img src={barinda} alt="react image" className="w-[28.93px] h-[28.93px] mt-7.75 ml-6.5" />
 
-            <p className="text-[22.25px] ml-[11.61px] mt-[31px]">ReactFacts</p>
+            <p className="text-[22.25px] ml-[11.61px] mt-7.75">ReactFacts</p>
         </header>
     )
 }
