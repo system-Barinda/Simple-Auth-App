c0862bb23cb39f24c7914f348237648f0f694b74
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-12 17:02:04 +0200

    header:complited

--- vite-project/src/pages/ReactFunFacts.tsx
+++ vite-project/src/pages/ReactFunFacts.tsx
@@ -2,8 +2,10 @@ import image from "../assets/react.svg"
 function Header(){
     return(
         <header className="h-[91px] w-[550px] bg-[#21222A] text-[#61DAFB]   flex">
-            <img src={image} alt="react image" className="w-[28.93px] h-[28.93px] t-[31px] l-[26px]" />
-            <p className="text-[22.25px] l-[61.69] t-[31px]">ReactFacts</p>
+
+            <img src={image} alt="react image" className="w-[28.93px] h-[28.93px] mt-[31px] ml-[26px]" />
+
+            <p className="text-[22.25px] ml-[11.61px] mt-[31px]">ReactFacts</p>
         </header>
     )
 }
