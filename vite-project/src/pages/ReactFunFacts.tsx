55c06acd4c37da412ee2b46b5abe30abeb9dfdec
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-12 16:48:39 +0200

    chore:adding style to the header components"

--- vite-project/src/pages/ReactFunFacts.tsx
+++ vite-project/src/pages/ReactFunFacts.tsx
@@ -1,13 +1,14 @@
+import image from "../assets/react.svg"
 function Header(){
     return(
-        <header>
-            <img src="" alt="" srcset="" />
+        <header className="h-[91px] w-[550px] bg-amber-300">
+            <img src={image} alt="react image" />
             <p>ReactFacts</p>
         </header>
     )
 }
 export default function ReactFunFacts(){
     return(
-        <h1>my name is barinda</h1>
+        <Header />
     )
 }
