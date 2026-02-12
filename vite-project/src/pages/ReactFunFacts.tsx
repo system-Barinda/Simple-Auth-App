95a024835a3ab40ab6b170855c58264f1a68c982
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-12 17:20:09 +0200

    done

--- vite-project/src/pages/ReactFunFacts.tsx
+++ vite-project/src/pages/ReactFunFacts.tsx
@@ -13,7 +13,16 @@ function Header(){
 function MainBody(){
     return(
      <div className="w-[681px] h-[459px] mt-[91px] bg-[#282D35]">
+         <p className="w-[424px] h-[41px] mt-[148px] ml-[27px] text-[39.06px] text-[#FFFFFF]">Fun facts about React</p>
+         <ol className="bodyText w-[390px] h-[190px] mt-[65px] ml-[74px] text-[#FFFFFF] flex-col space-y-3">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200K stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+          
 
+         </ol>
      </div>
     );
 }
