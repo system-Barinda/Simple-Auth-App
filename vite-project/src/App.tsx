1a6505d842fcd0a4145832b87f9c635992d4a326
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-12 18:00:26 +0200

    done

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,16 +1,18 @@
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
+    <Routes>
       <Navbar />
       <main className="p-4">
         <Dashboard />
       </main>
       <Footer />
-  
+  </Routes>
     </>
   );
 }
