3e3ed55b5d10cd1a7dbb08c3ae4b28237c15951d
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-12 17:48:11 +0200

    done:saperate all file in named function

---
+++ vite-project/src/components/layout/Header.tsx
@@ -0,0 +1,14 @@
+import barinda from "../assets/react.svg";
+function Header() {
+  return (
+    <header className="h-22.75 w-137.5 bg-[#21222A] text-[#61DAFB]  mx-auto flex ">
+      <img
+        src={barinda}
+        alt="react image"
+        className="w-[28.93px] h-[28.93px] mt-7.75 ml-6.5"
+      />
+
+      <p className="text-[22.25px] ml-[11.61px] mt-7.75">ReactFacts</p>
+    </header>
+  );
+}
--- vite-project/src/pages/ReactFunFacts.tsx
+++ vite-project/src/pages/ReactFunFacts.tsx
@@ -1,19 +1,3 @@
-import barinda from "../assets/react.svg";
-function Header() {
-  return (
-    <header className="h-22.75 w-137.5 bg-[#21222A] text-[#61DAFB]  mx-auto flex ">
-      <img
-        src={barinda}
-        alt="react image"
-        className="w-[28.93px] h-[28.93px] mt-7.75 ml-6.5"
-      />
-
-      <p className="text-[22.25px] ml-[11.61px] mt-7.75">ReactFacts</p>
-    </header>
-  );
-}
-
-
 export default function ReactFunFacts() {
   return (
     <div className="bg-black w-full h-161 ">
