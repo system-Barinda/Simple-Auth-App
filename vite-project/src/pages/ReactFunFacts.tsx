8136e1c6fb4457b55a37eb1785cd12471cd6fa30
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-12 16:43:04 +0200

    chore:add the components of the header

---
+++ vite-project/src/pages/ReactFunFacts.tsx
@@ -0,0 +1,13 @@
+function Header(){
+    return(
+        <header>
+            <img src="" alt="" srcset="" />
+            <p>ReactFacts</p>
+        </header>
+    )
+}
+export default function ReactFunFacts(){
+    return(
+        <h1>my name is barinda</h1>
+    )
+}
