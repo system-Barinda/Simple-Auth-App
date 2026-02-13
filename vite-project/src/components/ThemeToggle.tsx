9cbf76f291665e557a5feb5eab815f32ad8d5bcd
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-13 15:29:10 +0200

    done

--- vite-project/src/components/ThemeToggle.tsx
+++ vite-project/src/components/ThemeToggle.tsx
@@ -1,10 +1,18 @@
-export default function ThemeToggle({ darkMode, setDarkMode }) {
+type ThemeToggleProps = {
+  darkMode: boolean;
+  setDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function ThemeToggle({
+  darkMode,
+  setDarkMode,
+}: Readonly<ThemeToggleProps>) {
   return (
     <button
       onClick={() => setDarkMode(!darkMode)}
       className="px-3 py-1 rounded-xl text-sm font-semibold bg-gray-200 dark:bg-gray-600 dark:text-white"
     >
-      {darkMode ? "☀ Light" : "🌙 Dark"}
+      {darkMode ? "Light" : "Dark"}
     </button>
   );
 }
