d2cb4a7076b038f6fc1689c59d6a7aa6387e9c85
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-13 14:40:31 +0200

    done to make todolist

--- vite-project/src/components/layout/Navbar.tsx
+++ vite-project/src/components/layout/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const menu = ["home", "service", "fun-facts", "register"];
+const menu = ["home", "to do list", "fun-facts", "register"];
 
 function Navbar() {
   return (
